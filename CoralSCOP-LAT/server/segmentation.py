48d2abeb366b760bcb3b58076c477a0016609566
ykwongaq <ykwongaq@connect.ust.hk>
2024-11-01 12:09:13 +0800

    update code'

--- CoralSCOP-LAT/server/segmentation.py
+++ CoralSCOP-LAT/server/segmentation.py
@@ -24,8 +24,6 @@ class CoralSegmentation:
         device = ""
         if torch.cuda.is_available():
             device = "cuda"
-        elif torch.backends.mps.is_available():
-            device = "mps"
         else:
             device = "cpu"
 
